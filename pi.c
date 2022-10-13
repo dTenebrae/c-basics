ddd408f5d57234ac99531a9dbb5e3f05495f5241
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2022-10-13 12:47:31 +0300

    pi modification

--- pi.c
+++ pi.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 #define MAX_ITER 1000000000
-#define N 1E-9
+#define PRECISION 1E-5
+#define PI_CONST 3.14159265358979323846264338327950288419716939937510
 
-int main (int argc, char *argv[])
+void winter_pi(void)
+{
+        int a = 10000;
+        int b,d,e,g;
+        int c=2800;
+        int f[2801];
+        for(;b-c;)
+                f[b++]=a/5;
+        for(;d=0,g=c*2;c-=14, printf("%.4d",e+d/a),e=d%a)
+                for(b=c;d+=f[b]*a, f[b]=d%--g,d/=g--,--b;d*=b);
+
+}
+
+double series_pi(void)
 {
         int i = 0;
         int denom = 1;
@@ -19,10 +33,17 @@ int main (int argc, char *argv[])
                 i++;
                 prev_pi = pi;
                 
-        } while(i < MAX_ITER && error > N);
+        } while(i < MAX_ITER && error > PRECISION);
 
-        printf("Iterations: %d\n", i);
-        printf("pi: %.17g\n", pi);
+        return pi;
+
+}
+
+int main (int argc, char *argv[])
+{
+
+        printf("pi: %.17g\n", series_pi());
+        winter_pi();
         
         return 0;
 }
